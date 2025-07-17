79d111bafe01d6451aad2b9550d5890e4514da76
Charuhasini Krishnasamy <146703250+Charuhasini08@users.noreply.github.com>
2025-07-17 23:15:13 +0530

    Add files via upload

---
+++ Amazecare SQL file.sql
@@ -0,0 +1,6 @@
+create database amazecare;
+SELECT * FROM amazecare.appointment;
+SELECT * FROM amazecare.bill;
+SELECT * FROM amazecare.patient;
+SELECT * FROM amazecare.staff;
+SELECT * FROM amazecare.user;
